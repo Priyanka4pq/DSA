ce96cb10df42e1cbcc003919d6c6b96a02c2e4bd
Priyanka <priyankakumari4pq@gmail.com>
2025-12-19 12:24:22 +0530

    first commit

---
+++ 1st.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // cout << "iooooo jho vapple\n from priyanka world";
+    // cout <<"hello dude";
+    // cout <<"hello"<<"priyanka";
+
+    // int age =25;
+    // char grade= 'A';
+    // float PI= 3.14f;
+    // bool isSafe = true;
+    // double salary = 214.89;
+    // cout << "my age is "<<age <<"my grade is "<<grade << "and"<<PI <<"is it safe"<<isSafe<<"and my salary is"<<salary;
+
+
+    // char grade='A';
+    // int value = grade;
+    // cout<<value<<endl;    //it is implicit process, compiler krta hai.
+
+    // double price = 234.45;
+    // int newPrice = (int)price;
+    // cout << newPrice << endl;  //it is explicit process, programmer krta hai.
+
+    // int age;
+    // cout <<age;  //garbage value print hogi, jb hm variable ko koi value assign nhi krenge tb.
+
+    // int age;
+    // cout << "Enter the age : ";
+    // cin >> age;
+    // cout<<"your age is : "<<age<<endl;
+
+
+    //ARITHEMATIC OPERATOR
+    // int a=5, b=10;
+    // int sum= a+b;
+    // int mul= a*b;
+    // int div= b/a;
+    // int module= b%a;
+    // cout<<"sum is : "<<sum<<endl;
+    // cout<<"multiplication is : "<<mul<<endl;
+    // cout<<"division is : "<<div<<endl;
+    // cout<<"module is : "<<module<<endl;
+
+    // agr hme pura division chahiye toh
+    // int a=5;
+    // double b=2;
+    // cout<<"division is : "<<a/b;
+    // or by type casting
+    // cout << (5/double(2));
+    
+    // REALTIONAL OPERATOR
+    // cout<<(3<5)<<endl;  //true->1
+    // cout<<(7<5)<<endl;   //false->0
+    // cout<<(7<=7)<<endl;   
+    // cout<<(7!=17)<<endl; 
+    
+    // LOGICAL OPERATOR
+    // cout<<!(7!=17)<<endl;   
+    // cout<<!(3>1)<<endl;   
+
+    // cout <<((3<1) || (3>2))<<endl;  //true- one is true
+    // cout <<((3<1) && (3>2));  //false- both must be true
+   
+    // Q:1- Sum of two numbers-
+    // int a=7;
+    // int b=5;
+    // int sum = a+b;
+    // cout <<"Sum of two numbers is: "<<sum<<endl;
+
+    // USER'S INPUT-
+    // int a,b;
+    // cout<<"Enter a\n";
+    // cin>>a;
+    // cout << "Enter b\n";
+    // cin>>b;
+
+    // int sum;
+    // cout<<"Sum of two numbers is : "<<sum;
+
+    // UNARY OPERATOR---INCREMENT AND DECREMENT
+
+    // int a =10;
+    // int b = a++; //first kaam, then update
+    // cout << b<<endl;  //b=10
+    // cout << a <<endl;   //a =11
+
+    // int a= 10;
+    // int b=++a; 
+    // cout<<b<<endl;  //b=11
+    // cout<<a<<endl;  //a =11
+
+
+    // int a= 10;
+    // int b = --a;
+    // cout<<b<<endl;  
+    // cout<<a<<endl;
+
+    // int a= 10;
+    // int b = a--;
+    // cout<<b<<endl;  
+    // cout<<a<<endl;
+
+    // int n=45;
+    // if(n>=0){
+    //     cout<<"positive number";
+    // }else{
+    //     cout<<"negative";
+    // }
+
+
+    // int age;
+    // cout<<"enter your age : \n";
+    // cin>>age;
+    // if(age>=18){
+    //     cout<<"You are eligible to vote";
+    // }else{
+    //     cout<<"You are not eligible to vote";
+    // }
+
+
+    // int a;
+    // cout<<"Enter value of a : "<<endl;
+    // cin>>a;
+    // if(a%2==0){
+    //     cout<<"Even number";
+    // }else{
+    //     cout<<"Odd number";
+    // }
+
+
+    // int marks;
+    // cout<<"Enter marks"<<endl;
+    // cin>>marks;
+    // if(marks>90){
+    //     cout<<"Grade A";
+    // }else if(marks>=80){
+    //     cout<<"Grade B";
+    // }else{
+    //     cout<<"Grade C";
+    // }
+
+
+    char ch;
+    cout<<"Enter character"<<endl;
+    cin>>ch;
+
+    return 0;
+}
