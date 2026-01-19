5b151ef888e752d05fe48f8ad624cba7bd4ec75c
Priyanka <priyankakumari4pq@gmail.com>
2026-01-19 14:07:36 +0530

    Remove

--- RemoveAllReccurence.cpp
+++ RemoveAllReccurence.cpp
@@ -1,1 +1,21 @@
-#include <
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    string removeAllOccurence(string s,string part){
+        while(s.length() >0 && s.find(part) < s.length()){
+            s.erase(s.find(part), part.length());
+        }
+        return s;
+    };
+};
+int main(){
+    Solution obj;
+    string s= "daabcbaabcbc";
+    string part = "abc";
+
+    cout<<obj.removeAllOccurence(s,part)<<endl;
+
+    return 0;
+}
