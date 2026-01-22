be564e51ee40e286b20c521566d15cdab46fd5f7
Priyanka <priyankakumari4pq@gmail.com>
2026-01-22 13:17:22 +0530

    ok

--- MathForDsa.cpp
+++ MathForDsa.cpp
@@ -1,21 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// class Solution{
+//     public: 
+//     string isPrime(int n){
+//         if(n<=1) return "false";
+//         for(int i =2; i*i<=n; i++){
+//             if(n%i == 0) return "false";
+//         }
+//         return "true";
+//     }
+// };
+
+// int main(){
+//     // Math for DSA
+//     Solution obj;
+//     int n = 3;
+//     cout<<obj.isPrime(n)<<endl;
+//     return 0;
+// }
+
+
+//by sieve of eratosthenes
 class Solution{
-    public: 
-    bool isPrime(int n){
-        if(n<=1) return false;
-        for(int i =2; i*i<=n; i++){
-            if(n%i == 0) return false;
+    public:
+    int isPrime(int n){
+        if(n<=2) return 0;
+        vector<bool> isPrime(n, true);
+        
+        int count = 0;
+        for(int i =2; i<n; i++){
+            if(isPrime[i]){
+                count++;
+                for(int j = i*i; j<n; j+=i){
+                    isPrime[j]= false;
+                }
+            }
         }
-        return true;
+        return count;   
     }
 };
-
 int main(){
-    // Math for DSA
     Solution obj;
-    int n = 3;
+    int n = 10;
     cout<<obj.isPrime(n)<<endl;
-    return 0;
 }
