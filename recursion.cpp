c36f469150978fce1ca79a509ffa4de473093db1
Priyanka <priyankakumari4pq@gmail.com>
2026-02-04 13:07:34 +0530

    k

--- recursion.cpp
+++ recursion.cpp
@@ -2,18 +2,49 @@
 using namespace std;
 
 
-void printNums(int n){
+// void printNums(int n){
+//     //base case
+//     if(n == 0){
+//         return;
+//     }
+//     cout<<n<<" ";
+//     printNums(n-1);
+// }
+
+// int main(){
+//     printNums(5);
+//     return 0;
+// }
+
+
+//factorial of a number using recursion
+
+
+
+// int factorial(int n){
+//     //base case
+//     if(n == 0){
+//         return 1;
+//     }else{
+//         return n * factorial(n-1);
+//     }
+// }
+
+// int main(){
+//     int n= 5;
+//     cout<<factorial(n)<<endl;
+//     return 0;
+// }
+
+int Nsum(int n){
     //base case
-    if(n == 0){
-        cout<<"1";
-        return;
+    if(n == 1){
+        return 1;
     }
-    cout<<n<< " ";
-    printNums(n-1);
+    return n + Nsum(n-1);
 }
 
 int main(){
-
-    printNums(5);
+    cout<<Nsum(5)<<endl;
     return 0;
 }
