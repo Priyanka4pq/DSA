539342ca59a5d95406e22e26d6241d62c1536d66
Priyanka <priyankakumari4pq@gmail.com>
2026-02-19 22:00:30 +0530

    g

--- backtracking.cpp
+++ backtracking.cpp
@@ -71,35 +71,110 @@ using namespace std;
 
 //subset II - 90
 
-void uniqueSubsets(vector<int>& nums, vector<int>& ans, vector<vector<int>>& allSubsets, int index){
-    //base casee
-    if(index == nums.size()){
-        allSubsets.push_back(ans);
-        return;
+// void uniqueSubsets(vector<int>& nums, vector<int>& ans, vector<vector<int>>& allSubsets, int index){
+//     //base casee
+//     if(index == nums.size()){
+//         allSubsets.push_back(ans);
+//         return;
+//     }
+    
+//     //include the current element
+//     ans.push_back(nums[index]);
+//     uniqueSubsets(nums, ans, allSubsets, index+1);
+//     ans.pop_back(); //backtrack
+    
+//     //exclude the current element
+//     int idx = index+1;
+//     while(idx < nums.size() && nums[idx] == nums[idx-1]) idx++; //skip duplicates
+//     uniqueSubsets(nums, ans, allSubsets, idx);
+// }
+
+// int main(){
+//     vector<int> nums = {1,2,2};
+//     sort(nums.begin(), nums.end());
+//     vector<vector<int>> allSubsets; //store subsets
+//     vector<int> ans; //store current subset
+//     uniqueSubsets(nums, ans, allSubsets, 0);
+//     for(auto subset :allSubsets){
+//         for(int val : subset){
+//             cout<<val<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+//SUDUKO SOLVER - 37
+
+bool isSafe(vector<vector<char>>& board, int row, int col, char dig){
+    //check horizontal
+    for(int i=0; i<9; i++){
+        if(board[row][i] == dig)
+        return false;
     }
-    
-    //include the current element
-    ans.push_back(nums[index]);
-    uniqueSubsets(nums, ans, allSubsets, index+1);
-    ans.pop_back(); //backtrack
-    
-    //exclude the current element
-    int idx = index+1;
-    while(idx < nums.size() && nums[idx] == nums[idx-1]) idx++; //skip duplicates
-    uniqueSubsets(nums, ans, allSubsets, idx);
+
+    //check vertically
+    for(int j=0; j<board.size(); j++){
+        if(board[j][col]==dig){
+            return false;
+        }
+    }
+
+    //check 3*3 grid
+    int startRow = (row/3)*3;
+    int startCol = (col/3)*3;
+    for(int i=startRow; i<=startRow+2; i++){ 
+        for(int j=startCol; j<=startCol+2; j++){
+            if(board[i][j] == dig){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool helper(vector<vector<char>>& board, int row, int col){
+    //base case
+    if(row == board.size()) return true;
+    if(col == board[0].size()) return helper(board, row+1, 0);
+
+    if(board[row][col] != '.'){
+        return helper(board,row,col+1);
+    }
+
+    for(char dig='1'; dig<='9'; dig++){
+        if(isSafe(board,row,col,dig)){
+            board[row][col] = dig;
+            if(helper(board, row, col+1)) return true;
+            board[row][col] = '.'; //backtrack
+        }
+    }
+    return false;
 }
 
 int main(){
-    vector<int> nums = {1,2,2};
-    sort(nums.begin(), nums.end());
-    vector<vector<int>> allSubsets; //store subsets
-    vector<int> ans; //store current subset
-    uniqueSubsets(nums, ans, allSubsets, 0);
-    for(auto subset :allSubsets){
-        for(int val : subset){
-            cout<<val<<" ";
+    vector<vector<char>> board = {
+        {'5','3','.','.','7','.','.','.','.'},
+        {'6','.','.','1','9','5','.','.','.'},
+        {'.','9','8','.','.','.','.','6','.'},
+        {'8','.','.','.','6','.','.','.','3'},
+        {'4','.','.','8','.','3','.','.','1'},
+        {'7','.','.','.','2','.','.','.','6'},
+        {'.','6','.','.','.','.','2','8','.'},
+        {'.','.','.','4','1','9','.','.','5'},
+        {'.','.','.','.','8','.','.','7','9'}
+    };
+      if(helper(board, 0, 0)){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
         }
-        cout<<endl;
+    } else {
+        cout << "No solution exists" << endl;
     }
     return 0;
 }
