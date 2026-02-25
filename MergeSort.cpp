6d8fb82a15bc75d6d017d649fe2977e87bf83a23
Priyanka <priyankakumari4pq@gmail.com>
2026-02-25 22:54:13 +0530

    f

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -42,47 +42,97 @@ using namespace std;
 
 
 
-void merge(vector<int>& arr, int st, int mid, int end){
+// void merge(vector<int>& arr, int st, int mid, int end){
+//     vector<int> temp;
+//     int i = st, j = mid+1;
+
+//     while(i <= mid && j <=end){
+//         // if(arr[i]<=arr[j]){   //increasing order
+//         if(arr[i]>=arr[j]){   //decreasing order
+//             temp.push_back(arr[i]);
+//             i++;
+//         }
+//         else{
+//             temp.push_back(arr[j]);
+//             j++;
+//         }
+//     }
+//     while(i <=mid){
+//         temp.push_back(arr[i]);
+//         i++;
+//     }
+//     while(j <= end){
+//         temp.push_back(arr[j]);
+//         j++;
+//     }
+//     for(int idx = 0; idx < temp.size(); idx++){
+//         arr[idx+st] = temp[idx];
+//     }
+// }
+// void mergeSort(vector<int>& arr, int st, int end){
+//     if(st< end){
+//         int mid = st+(end-st)/2;
+//         mergeSort(arr, st, mid);  //left half
+//         mergeSort(arr, mid+1, end); //right half
+
+//         merge(arr, st, mid, end);
+//     }
+// }
+// int main(){
+//     vector<int> arr = {38,27,43,3,9,82,10};
+//     mergeSort(arr, 0 , arr.size()-1);
+//     for(int x : arr){
+//         cout<<x<<" ";
+//     }
+//     return 0;
+// }
+
+
+
+
+//inversion count
+
+
+int merge(vector<int>& arr, int st ,int mid, int end){
     vector<int> temp;
     int i = st, j = mid+1;
-
-    while(i <= mid && j <=end){
-        // if(arr[i]<=arr[j]){   //increasing order
-        if(arr[i]>=arr[j]){   //decreasing order
+    int count = 0;
+    while(i<=mid && j<=end){
+        if(arr[i]<arr[j]){
             temp.push_back(arr[i]);
             i++;
-        }
-        else{
+        }else{
             temp.push_back(arr[j]);
+            count += (mid - i +1); // all the elements from i to mid will be greater than arr[j]
             j++;
         }
     }
-    while(i <=mid){
+    while(i<=mid){
         temp.push_back(arr[i]);
         i++;
     }
-    while(j <= end){
+    while(j<=end){
         temp.push_back(arr[j]);
         j++;
     }
-    for(int idx = 0; idx < temp.size(); idx++){
+    for(int idx = 0; idx <temp.size(); idx++){
         arr[idx+st] = temp[idx];
     }
+    return count;
 }
-void mergeSort(vector<int>& arr, int st, int end){
-    if(st< end){
-        int mid = st+(end-st)/2;
-        mergeSort(arr, st, mid);  //left half
-        mergeSort(arr, mid+1, end); //right half
 
-        merge(arr, st, mid, end);
+int inversionCount(vector<int>& arr, int st , int end){
+    while(st<end){
+        int mid = st+(end-st)/2;
+        int leftCount = inversionCount(arr,st,mid);
+        int rightCount = inversionCount(arr,mid+1, end);
+        int mergeCount = merge(arr, st, mid, end);
+        return leftCount + rightCount + mergeCount;
     }
+    return 0;
 }
 int main(){
-    vector<int> arr = {38,27,43,3,9,82,10};
-    mergeSort(arr, 0 , arr.size()-1);
-    for(int x : arr){
-        cout<<x<<" ";
-    }
+    vector<int> arr = {6,3,5,2,7};
+    cout<<"Inversion count is : "<<inversionCount(arr, 0, arr.size()-1)<<endl;
     return 0;
 }
