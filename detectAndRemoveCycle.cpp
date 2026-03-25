04b76ed7b21a658eb95b40aa140bbe2a19df0a87
Priyanka <priyankakumari4pq@gmail.com>
2026-03-25 17:30:49 +0530

    f

--- detectAndRemoveCycle.cpp
+++ detectAndRemoveCycle.cpp
@@ -24,23 +24,70 @@ public:
     {
         head = tail = NULL;
     }
-    void push_back(int val){
-        Node* newNode = new Node(val);
-        if(head == NULL){
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
             head = tail = newNode;
-        }else{
+        }
+        else
+        {
             tail->next = newNode;
             tail = newNode;
         }
     }
 
-    void printll(){
-        Node* temp = head;
-        while(temp != NULL){
-            cout<<temp->data<<"->";
+    void createCycle(int pos)
+    {
+        if (pos <= -1)
+            return;
+        Node *temp = head;
+        Node *cycleNode = NULL;
+        int index = 0;
+        while(temp->next != NULL){
+            if(pos == index){
+                cycleNode = temp;
+            }
+            temp = temp->next;
+            index++;
+        }
+        if(cycleNode){
+            temp->next = cycleNode;
+        }
+    }
+
+    Node *detectCycle(){
+        Node* slow = head;
+        Node* fast = head;
+        int cycle = false;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                cycle = true;
+                break;
+            }
+        }
+        if(!cycle){
+            return NULL;
+        }
+        slow = head;
+        while(slow!= fast){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return slow;
+    }
+    void printll()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << "->";
             temp = temp->next;
         }
-        cout<<"NULL"<<endl;
+        cout << "NULL" << endl;
     }
 };
 
@@ -59,6 +106,18 @@ int main()
         ll.push_back(x);
     }
     ll.printll();
+
+    int pos;
+    cout << "Enter cycle position : ";
+    cin >> pos;
+
+    ll.createCycle(pos);
+    Node* result = ll.detectCycle();
+    if(result){
+        cout<<"cycle start at node with value: "<<result->data<<endl;
+    }else{
+        cout<<"No cycle Detected"<<endl;
+    }
 
     return 0;
 }
