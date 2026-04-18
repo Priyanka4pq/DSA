bdbf69f4f2e0836fb00d1f8d91c4c8e5d129e0fd
Priyanka <priyankakumari4pq@gmail.com>
2026-04-18 16:05:06 +0530

    queue

--- Queue.cpp
+++ Queue.cpp
@@ -1,84 +1,80 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<queue>
 using namespace std;
 
-class Node
-{
-public:
-    int data;
-    Node *next;
-    Node(int val)
-    {
-        data = val;
-        next = NULL;
-    }
-};
+// class Node
+// {
+// public:
+//     int data;
+//     Node *next;
+//     Node(int val)
+//     {
+//         data = val;
+//         next = NULL;
+//     }
+// };
 
-class Queue
-{
-    Node *head;
-    Node *tail;
+// class Queue
+// {
+//     Node *head;
+//     Node *tail;
 
-public:
-    Queue()
-    {
-        head = tail = NULL;
-    }
-    void push(int data)
-    {
-        Node *newNode = new Node(data);
-        if (empty())
-        {
-            head = tail = newNode;
-        }
-        else
-        {
-            tail->next = newNode;
-            tail = newNode;
-        }
-    }
-    void pop()
-    {
-        if (empty())
-            return;
+// public:
+//     Queue()
+//     {
+//         head = tail = NULL;
+//     }
+//     void push(int data)
+//     {
+//         Node *newNode = new Node(data);
+//         if (empty())
+//         {
+//             head = tail = newNode;
+//         }
+//         else
+//         {
+//             tail->next = newNode;
+//             tail = newNode;
+//         }
+//     }
+//     void pop()
+//     {
+//         if (empty())
+//             return;
 
-        Node *temp = head;
-        head = head->next;
+//         Node *temp = head;
+//         head = head->next;
 
-        if (head == NULL)
-        { // queue became empty
-            tail = NULL;
-        }
+//         if (head == NULL)
+//         { // queue became empty
+//             tail = NULL;
+//         }
 
-        delete temp;
-    }
+//         delete temp;
+//     }
 
-    int front()
-    {
-        if (empty())
-        {
-            cout << "Queue is empty\n";
-            return -1; // or throw exception
-        }
-        return head->data;
-    }
+//     int front()
+//     {
+//         if (empty())
+//         {
+//             cout << "Queue is empty\n";
+//             return -1; // or throw exception
+//         }
+//         return head->data;
+//     }
 
-    bool empty()
-    {
-        return head == NULL;
-    }
-    // int print(){
-    //     Node* temp = head;
-    //     while(temp != NULL){
-    //         cout<<temp->data<<" ";
-    //         temp = temp->next;
-    //     }
-    //     cout<<endl;
-    // }
-};
+//     bool empty()
+//     {
+//         return head == NULL;
+//     }
+// };
 
 int main()
 {
-    Queue q;
+    queue<int> q; // using STL queue
+    // Queue q;
     for (int i = 1; i <= 5; i++)
     {
         q.push(i);
